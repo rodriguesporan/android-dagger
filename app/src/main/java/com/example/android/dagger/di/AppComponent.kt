b9aaf17819336553ad499807fdd75e688828d8e9
rodriguesporan <rodriguesporan@gmail.com>
2021-09-15 07:38:28 -0300

    feat: created StorageModule @Module @Binds

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,7 +3,7 @@ package com.example.android.dagger.di
 import com.example.android.dagger.registration.RegistrationActivity
 import dagger.Component
 
-@Component
+@Component(modules = [StorageModule::class])
 interface AppComponent {
     fun inject(activity: RegistrationActivity)
 }

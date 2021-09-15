4228a1bf4b8c3e67adba5be6b2789bfa97293e28
rodriguesporan <rodriguesporan@gmail.com>
2021-09-15 10:00:48 -0300

    feat: Factory to create instances of the AppComponent

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,9 +1,19 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
 @Component(modules = [StorageModule::class])
 interface AppComponent {
+
+    // Factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        // With @BindsInstance, the Context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
     fun inject(activity: RegistrationActivity)
 }

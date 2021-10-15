4776ffafe541dac53b7a61657aa6683915e63656
rodriguesporan <rodriguesporan@gmail.com>
2021-10-14 23:05:59 -0300

    feat: added Injecting the graph into Fragments

--- app/src/main/java/com/example/android/dagger/registration/termsandconditions/TermsAndConditionsFragment.kt
+++ app/src/main/java/com/example/android/dagger/registration/termsandconditions/TermsAndConditionsFragment.kt
@@ -16,19 +16,22 @@
 
 package com.example.android.dagger.registration.termsandconditions
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.android.dagger.MyApplication
 import com.example.android.dagger.R
 import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.registration.RegistrationViewModel
+import javax.inject.Inject
 
 class TermsAndConditionsFragment : Fragment() {
 
-    private lateinit var registrationViewModel: RegistrationViewModel
+    @Inject lateinit var registrationViewModel: RegistrationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,5 +48,11 @@ class TermsAndConditionsFragment : Fragment() {
         }
 
         return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        (requireActivity().application as MyApplication).appComponent.inject(this)
     }
 }
